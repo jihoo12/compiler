419a086fe38e295f1e90c417d0874826ab577cc7
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-05 02:26:15 +0000

    backend.c update

--- backend.c
+++ backend.c
@@ -1,3 +1,6 @@
 #define syscall 0x0F05
 #define leave 0xC9
 #define pop(reg) (0x58 + (reg))
+#define pushimm8(imm8) 0x6A##imm8
+#define pushimm16-64(imm16-64) 0x68##imm16-64
+#define pushreg(reg) (0x50 + (reg))
