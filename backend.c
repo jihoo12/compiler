16c9e2455badbe175519292f9e3119c440883cea
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-05 02:32:35 +0000

    backend.c update

--- backend.c
+++ backend.c
@@ -4,3 +4,7 @@
 #define pushimm8(imm8) 0x6A##imm8
 #define pushimm16-64(imm16-64) 0x68##imm16-64
 #define pushreg(reg) (0x50 + (reg))
+#define ret 0xC3
+#define retimm16(imm16) 0xC2##imm16
+#define retfar 0xCB
+#define retfarimm16(imm16) 0xCA##imm16
