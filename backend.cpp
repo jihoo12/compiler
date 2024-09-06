d01b8ee3a57bad8424384aef258341b06d3e975e
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-06 06:03:19 +0000

    backend.cpp update

--- backend.cpp
+++ backend.cpp
@@ -1,12 +1,14 @@
 class Backend {
 private:
-std::vector<string> out;
+std::vector<char> out;
 char leave = 0xC9;
 char syscall = 0x0F05;
 char init = 0x554889E5;
 public:
 inline void movregreg(char num) {
-
+out.push_back(0x48);
+out.push_back(0x89);
+out.push_back(num);
 }
 inline void movreg() {
 }
