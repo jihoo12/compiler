1d6697f34fb34d142c523129d618200ed1f913f9
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-06 04:37:46 +0000

    backend.cpp update

--- backend.cpp
+++ backend.cpp
@@ -5,7 +5,8 @@ char leave = 0xC9;
 char syscall = 0x0F05;
 char init = 0x554889E5;
 public:
-inline void movregreg(char reg1,char reg2) {
+inline void movregreg(char num) {
+
 }
 inline void movreg() {
 }
