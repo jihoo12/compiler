bf531a162f42c997f413755d1b1f8f08cbdf3ec7
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-10 01:38:22 +0000

    i don't know what i want

--- frontend.cpp
+++ frontend.cpp
@@ -1,24 +1,31 @@
 #include <iostream>
-#include <string>
 #include <fstream>
 #include <vector>
+
 int main(int argc, char* argv[]) {
     if (argc < 2) {
         std::cout << "No input file" << std::endl;
         return 1;
     }
-    std::ifstream inFile(argv[2]);
-    std::string line;
-    std::vector<std::string> lines;
-
-    if (inFile.is_open()) {
-        while (std::getline(inFile, line)) {
-            lines.push_back(line);
-        }
-        inFile.close();
-    } else {
+    
+    std::ifstream inFile(argv[1], std::ios::binary);
+    if (!inFile) {
         std::cerr << "can't open file" << std::endl;
         return 1;
     }
+
+    std::vector<char> chars;
+    char ch;
+    while (inFile.get(ch)) {
+        chars.push_back(ch);
+    }
+    inFile.close();
+    for (int i=0;i<chars.size()) {
+        switch (chars[i]) {
+           
+        }
+    }
+    std::cout << std::endl;
+
     return 0;
 }
