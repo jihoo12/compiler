05f1022735948aed9559c8dfd0ea996dbd68f57d
jihoo <134384139+jihoo12@users.noreply.github.com>
2024-09-10 01:22:57 +0000

    frontend.cpp update

--- frontend.cpp
+++ frontend.cpp
@@ -1,10 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-int main(int argc ,char *argv[]) {
-if (argc<2) {
-printf("no input file");
-return 1;
-exit 0;
-}
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cout << "No input file" << std::endl;
+        return 1;
+    }
+    std::ifstream inFile(argv[2]);
+    std::string line;
+    std::vector<std::string> lines;
 
+    if (inFile.is_open()) {
+        while (std::getline(inFile, line)) {
+            lines.push_back(line);
+        }
+        inFile.close();
+    } else {
+        std::cerr << "can't open file" << std::endl;
+        return 1;
+    }
+    return 0;
 }
